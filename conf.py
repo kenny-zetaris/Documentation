b6583a34f269fc8694eb476a8c45992e4651c69a
varuns-zetaris <varun.shaji@zetaris.com>
2019-08-16 11:33:40 +1000

    Update conf.py

--- conf.py
+++ conf.py
@@ -84,8 +84,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-#html_theme = "default"
+#html_theme = "sphinx_rtd_theme"
+html_theme = "default"
 html_theme_options = {
     #"logo_only": "true",
     #"relbarbgcolor": "black"
